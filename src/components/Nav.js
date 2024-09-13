f01cce43d86e68c18aff655dba15c2c866ccc84e
alpefesezer <72073715+alpefesezer@users.noreply.github.com>
2024-09-13 12:17:19 +0300

    added semantic tags

---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+const Nav = () => {
+  return (
+    <nav>
+      <li>
+        <ul>
+          <a href="index.html">Home</a>
+        </ul>
+        <ul>
+          <a href="about.html">About</a>
+        </ul>
+        <ul>
+          <a href="menu.html">Menu</a>
+        </ul>
+        <ul>
+          <a href="reversations.html">Reservations</a>
+        </ul>
+        <ul>
+          <a href="order-online.html">Order Online</a>
+        </ul>
+        <ul>
+          <a href="login.html">Login</a>
+        </ul>
+      </li>
+    </nav>
+  );
+};
+
+export default Nav;
