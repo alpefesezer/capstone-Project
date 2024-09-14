7a4ae1f44929a65a8ad10e73ed3504ec183742ba
alpefesezer <72073715+alpefesezer@users.noreply.github.com>
2024-09-14 11:55:44 +0300

    navbar ve main componenti iyileştirildi.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,32 @@ import logo from "../assets/Logo.png";
 
 const Header = () => {
   return (
-    <header>
-      <img src={logo} alt="logo" />
+    <header className="header-container">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <nav className="nav-menu">
+        <ul>
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="about.html">About</a>
+          </li>
+          <li>
+            <a href="menu.html">Menu</a>
+          </li>
+          <li>
+            <a href="reservations.html">Reservations</a>
+          </li>
+          <li>
+            <a href="order-online.html">Order Online</a>
+          </li>
+          <li>
+            <a href="login.html">Login</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,43 @@
+import photo from "../assets/restauranfood.jpg";
+
 const Main = () => {
-  return <main></main>;
+  return (
+    <main>
+      <div className="first-content">
+        <div className="headers">
+          <div>
+            <h1 className="header-title">Little Lemon</h1>
+            <p
+              style={{
+                color: "white",
+                marginTop: 0,
+                fontSize: 20,
+                fontFamily: "Inter",
+                marginBottom: 60,
+              }}
+            >
+              Chicago
+            </p>
+            <p
+              style={{
+                color: "white",
+                marginTop: 0,
+                marginBottom: 0,
+                fontFamily: "Inter",
+              }}
+            >
+              Lorem ipsum dolor sit amet,
+            </p>
+            <p style={{ color: "white", marginTop: 0, fontFamily: "Inter" }}>
+              consectetur adipiscing elit,
+            </p>
+            <button className="reserve-button">Reserve a Table</button>
+          </div>
+        </div>
+        <div className="first-image"></div>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
